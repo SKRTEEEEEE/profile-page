0b125926442f1a9d84767788b9249e04989bbd8e
SKRTEEEEEE <adanreh.m@gmail.com>
2024-05-19 10:01:11 +0200

    feat(cron): Create cron function with secret

--- app/api/github/cron/route.ts
+++ app/api/github/cron/route.ts
@@ -1,14 +1,26 @@
 import { testPeticionRepos } from "@/actions"
-import { NextResponse } from "next/server"
+// import { NextResponse } from "next/server"
 
 
-//Falta configurar el secret del cron para que no cualquiera accediendo a la ruta active la función
-export async function GET() {
-    const result = "Hello, World! This is CRON route."
-    await testPeticionRepos()
+// //Falta configurar el secret del cron para que no cualquiera accediendo a la ruta active la función
+// export async function GET() {
+//     const result = "Hello, World! This is CRON route."
+//     await testPeticionRepos()
 
     
 
-    return NextResponse.json({ data: result })
+//     return NextResponse.json({ data: result })
 
+// }
+import type { NextRequest } from 'next/server';
+ 
+export async function GET(request: NextRequest) {
+  const authHeader = request.headers.get('authorization');
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response('Unauthorized', {
+      status: 401,
+    });
+  }
+    await testPeticionRepos();
+  return Response.json({ success: true });
 }
