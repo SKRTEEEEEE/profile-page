fb27c5cbf9bb1cabb46330f31ecc1614839f0b8e
SKRTEEEEEE <adanreh.m@gmail.com>
2024-05-22 21:19:18 +0200

    refactor(admin): Update create techs form

--- components/routes/formulario-techs-create.tsx
+++ components/routes/formulario-techs-create.tsx
@@ -4,7 +4,7 @@ import { publicarFwALeng, publicarLeng, publicarLibAFw } from "@/actions/badges"
 import { IFrameworkDispo, ILenguajeDispo } from "@/app/(routes)/test/form/page";
 import techBadges from "@/data/slugs";
 import { IFrameworkForm, ILenguajeForm, ILibreriaForm } from "@/types";
-import {  Autocomplete, AutocompleteItem, Button, Input, Radio, RadioGroup, Slider } from "@nextui-org/react";
+import { Autocomplete, AutocompleteItem, Button, Input, Radio, RadioGroup, Slider } from "@nextui-org/react";
 import { useState } from "react";
 import { useAsyncList } from "@react-stately/data";
 
@@ -15,111 +15,92 @@ interface FormularioTechsProps {
 }
 type TechBadge = {
     name: string;
-  };
+};
 
 
-const FormularioCreateTechs: React.FC<FormularioTechsProps> = ({dispoLeng, dispoFw}) => {
-    let list = useAsyncList<TechBadge>({
-        async load({ signal, filterText }) {
-          // Filtra la lista local según el texto ingresado
-          let searchText = filterText ? filterText.toLowerCase() : "";
-
-      // Filtra la lista local según el texto ingresado
-      let filteredItems = techBadges.filter(item =>
-        item.name.toLowerCase().includes(searchText)
-      );
-    
-          return {
-            items: filteredItems,
-          };
-        },
-      });
-    
-    // console.log("fw: ", techBadges)
+const FormularioCreateTechs: React.FC<FormularioTechsProps> = ({ dispoLeng, dispoFw }) => {
     const [selectedCat, setSelectedCat] = useState<string>("lenguaje");
     const [inputValue, setInputValue] = useState<string>('');
     const [serverResponse, setServerResponse] = useState<{ success: boolean, message: string } | null>(null);
     const [isLoading, setIsLoading] = useState<boolean>(false);
+    let list = useAsyncList<TechBadge>({
+        async load({ signal, filterText }) {
+            // Filtra la lista local según el texto ingresado
+            let searchText = filterText ? filterText.toLowerCase() : "";
 
-  const handleCopyClick = () => {
-    setInputValue(list.filterText);
-  };
+            // Filtra la lista local según el texto ingresado
+            let filteredItems = techBadges.filter(item =>
+                item.name.toLowerCase().includes(searchText)
+            );
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value);
-  };
-    const techMessage = `Busca un(a) ${selectedCat} con logo`
-    return(
-        <form onSubmit={
-            async (ev) => {
-            
-            ev.preventDefault();
-            setIsLoading(true);
-                try {
-                    const formData = new FormData(ev.target as HTMLFormElement);
-            const data = Object.fromEntries(formData.entries());
-            console.log(data);
+            return {
+                items: filteredItems,
+            };
+        },
+    });
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+    };
+    //   const handleCopyClick = () => {
+    //     setInputValue(list.filterText);
+    //   };
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setIsLoading(true);
+
+        try {
+            const data: any = {};
+            const formElements = Array.prototype.slice.call(e.currentTarget.elements) as HTMLInputElement[];
+            formElements.forEach((field: HTMLInputElement) => {
+                if (field.name) {
+                    data[field.name] = field.value;
+                }
+            });
+
+
+            const requestData = {
+                name: data.name,
+                afinidad: parseInt(data.afinidad, 10),
+                badge: data.badge,
+                preferencia: parseInt(data.preferencia, 10),
+                color: data.color,
+                experiencia: parseFloat(data.experiencia),
+                lenguajeTo: selectedCat === "framework" || selectedCat === "libreria" ? data.lenguajeTo : undefined,
+                frameworkTo: selectedCat === "libreria" ? data.frameworkTo : undefined,
+            };
+
+            let response;
             switch (selectedCat) {
                 case "lenguaje":
-                    const transformedData: ILenguajeForm = {
-                        name: data.name as string,
-                        afinidad: parseInt(data.afinidad as string, 10),
-                        badge: data.badge as string,
-                        preferencia: parseInt(data.preferencia as string, 10),
-                        color: data.color as string,
-                        experiencia: parseFloat(data.experiencia as string),
-                    };
-                    const resLeng = await publicarLeng(transformedData);
-                    setServerResponse(resLeng);
+                    response = await publicarLeng(requestData as ILenguajeForm);
                     break;
                 case "framework":
-                    const transformedDataFw: IFrameworkForm = {
-                        name: data.name as string,
-                        afinidad: parseInt(data.afinidad as string, 10),
-                        badge: data.badge as string,
-                        preferencia: parseInt(data.preferencia as string, 10),
-                        color: data.color as string,
-                        experiencia: parseFloat(data.experiencia as string),
-                        lenguajeTo: data.lenguajeTo as string,
-                        // frameworks: []  // As specified, no frameworks will be included
-
-                    };
-                    const resFw = await publicarFwALeng(transformedDataFw);
-                    console.log("resFw: ", resFw);
-                    
-                    setServerResponse(resFw);
+                    response = await publicarFwALeng(requestData as IFrameworkForm);
                     break;
                 case "libreria":
-                    const transformedDataLib: ILibreriaForm = {
-                        name: data.name as string,
-                        afinidad: parseInt(data.afinidad as string, 10),
-                        badge: data.badge as string,
-                        preferencia: parseInt(data.preferencia as string, 10),
-                        color: data.color as string,
-                        experiencia: parseFloat(data.experiencia as string),
-                        lenguajeTo: data.lenguajeTo as string,
-                        frameworkTo: data.frameworkTo as string,
-                        // frameworks: []  // As specified, no frameworks will be included
-                    };
-                    const resLib = await publicarLibAFw(transformedDataLib);
-                    setServerResponse(resLib);
+                    response = await publicarLibAFw(requestData as ILibreriaForm);
                     break;
                 default:
                     throw new Error("Categoría no reconocida");
-
-
             }
-                } catch (error) {
-                    console.log(error)
-                } finally {
-                    console.log("finally", serverResponse?.success)
-                    if(serverResponse?.success === true){
-                        alert(`Congrats🤖🚀🕵️\n${serverResponse?.message}`)
-                        window.location.href="/admin/techs"}
-                        if(serverResponse?.success === false)alert(`Oops🤖🙋‍♂️🕵️\n${serverResponse?.message}`)
-                    setIsLoading(false);
-                }
-        }}>
+            setServerResponse(response);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setIsLoading(false);
+            if (serverResponse?.success === true) {
+                alert(`¡Felicidades! ${serverResponse.message}`);
+                window.location.href = "/admin/techs";
+            } else if (serverResponse?.success === false) {
+                alert(`Oops! ${serverResponse.message}`);
+            }
+        }
+    };
+
+
+    const techMessage = `Busca un(a) ${selectedCat} con logo`
+    return (
+        <form onSubmit={handleSubmit}>
             <h1>Form input test</h1>
             <RadioGroup
                 label="Selecciona a que categoría pertenece la tecnología"
@@ -135,23 +116,23 @@ const FormularioCreateTechs: React.FC<FormularioTechsProps> = ({dispoLeng, dispo
             </RadioGroup>
 
             <Autocomplete
-      className="max-w-xs"
-      inputValue={list.filterText}
-      isLoading={list.isLoading}
-      items={list.items}
-      label={techMessage}
-      placeholder="Escribe para buscar..."
-      variant="bordered"
-      onInputChange={list.setFilterText}
-    >
-      {(item) => (
-        <AutocompleteItem key={item.name} className="capitalize">
-          {item.name}
-        </AutocompleteItem>
-      )}
-    </Autocomplete>
-            
-             <Input isRequired name="name" type="string" label="Tecnología" description="Nombre que se pueda usar como logo en los badges de shields.io" size="lg" value={inputValue} onChange={handleInputChange}/> <Button onClick={handleCopyClick}>Copiar</Button>
+                className="max-w-xs"
+                inputValue={list.filterText}
+                isLoading={list.isLoading}
+                items={list.items}
+                label={techMessage}
+                placeholder="Escribe para buscar..."
+                variant="bordered"
+                onInputChange={list.setFilterText}
+            >
+                {(item) => (
+                    <AutocompleteItem key={item.name} className="capitalize">
+                        {item.name}
+                    </AutocompleteItem>
+                )}
+            </Autocomplete>
+
+            <Input isRequired name="name" type="string" label="Tecnología" description="Nombre que se pueda usar como logo en los badges de shields.io" size="lg" value={inputValue} onChange={handleInputChange} /> <Button onClick={() => setInputValue(list.filterText)}>Copiar</Button>
             {(selectedCat === "framework" || selectedCat == "libreria") &&
 
                 <Autocomplete
@@ -166,10 +147,10 @@ const FormularioCreateTechs: React.FC<FormularioTechsProps> = ({dispoLeng, dispo
                 >
                     {(lenguaje) => <AutocompleteItem key={lenguaje.name}>{lenguaje.name}</AutocompleteItem>}
                 </Autocomplete>
-                }
-            {(selectedCat === "libreria") && 
+            }
+            {(selectedCat === "libreria") &&
 
-            <Autocomplete
+                <Autocomplete
                     isRequired
                     variant="bordered"
                     defaultItems={dispoFw}
@@ -182,7 +163,7 @@ const FormularioCreateTechs: React.FC<FormularioTechsProps> = ({dispoLeng, dispo
                     {(lenguaje) => <AutocompleteItem key={lenguaje.name}>{lenguaje.name}</AutocompleteItem>}
                 </Autocomplete>
             }
-            <Input isRequired name="preferencia" type="number" label="Preferencia" description="Orden en categoría" size="sm" className="max-w-[120px]"/>
+            <Input isRequired name="preferencia" type="number" label="Preferencia" description="Orden en categoría" size="sm" className="max-w-[120px]" />
             <Input isRequired name="badge" type="string" label="Badge MD" description="Badge para usar en markdown" size="md" />
             <Input isRequired name="color" type="color-hex" label="Color" description="Color que se pueda usar como logo en los badges de shields.io" size="sm" variant="underlined" labelPlacement="outside-left" />
             <Slider
@@ -240,10 +221,10 @@ const FormularioCreateTechs: React.FC<FormularioTechsProps> = ({dispoLeng, dispo
                 className="max-w-md"
             />
             {isLoading ? (
-      <p>Cargando...</p>
-    ) : (
-      <Button type="submit">Enviar</Button>
-    )}
+                <p>Cargando...</p>
+            ) : (
+                <Button type="submit">Enviar</Button>
+            )}
         </form>
     )
 }
