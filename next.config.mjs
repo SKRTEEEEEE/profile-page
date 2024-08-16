d1b706ac8d8aa0c2eebf91b2c739055a8fe6711a
SKRTEEEEEE <adanreh.m@gmail.com>
2024-08-16 22:30:10 +0200

    fix(app): Add pages for nextra

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-import withNextra from 'nextra';
-
-const nextraConfig = withNextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx'
-});
-
-export default nextraConfig;
