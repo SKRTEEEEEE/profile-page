b2f95b0ec27c838a2115ac94f6c471350b7aace3
SKRTEEEEEE <adanreh.m@gmail.com>
2024-09-12 20:14:35 +0200

    fix(tarifas): Some error with name files

--- src/components/subscription-plans-card.tsx
+++ src/components/subscription-plans-card.tsx
@@ -11,63 +11,9 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
-import PlainsComparisonTable from "./plains-comparison-table"
+import { plans } from "@/app/(main)/academia/tarifas/page"
 
-const plans = [
-  {
-    name: "Plan Gratuito",
-    description: "Perfecto para comenzar",
-    price: "0€",
-    period: "mes",
-    subtitle: "🎉 Subscribe para comenzar !",
-    features: [
-      "Acceso a cursos profesionales",
-      "Acceso a multiples ejercicios",
-      "Foro de la comunidad",
-      "Recursos de aprendizaje básicos",
-      // "Actualizaciones mensuales de contenido",
-    ],
-  },
-  {
-    name: "Plan Básico",
-    description: "Ideal para estudiantes dedicados",
-    price: "9.99€",
-    period: "mes",
-    subtitle: "💶 Ahora, primer mes GRATIS !",
-    features: [
-      "Todo lo incluido en el Plan Gratuito",
-      "Acceso a cursos exclusivos",
-      "Descuentos exclusivos de la comunidad",
-      "Soporte por email",
-    ],
-    extraFeatures: [
-      "Acceso a foro exclusivo",
-      "Acceso a ejercicios exclusivos",
-      // "Descuentos en suscripciones a cursos",
-      "Recursos de aprendizaje gratuitos/incluidos"
-    ],
-  },
-  {
-    name: "Plan Premium",
-    description: "Para profesionales y expertos",
-    price: "19.99€",
-    period: "mes",
-    subtitle: "🧰 Primer mes, solo 10€ !",
-    features: [
-      "Todo lo incluido en el Plan Básico",
-      // "Mentorías personalizadas incluidas",
-      "Soporte prioritario 24/7",
-      "Acceso a todos los cursos",
-      "Acceso a todos los ejercicios",
-      // "Suscripción a cursos incluida",
 
-    ],
-    extraFeatures: [
-      "Voto para elección de próximos ejercicios",
-      "Descarga de contenido offline",
-    ],
-  },
-]
 
 function FeatureList({ features }: { features: string[] }) {
   return (
@@ -97,7 +43,7 @@ function PremiumPlainButton(){
   )
 }
 
-function PlanCard({ plan }: { plan: typeof plans[number] }) {
+export function SubscriptionPlanCard({ plan }: { plan: typeof plans[number] }) {
   return (
     <Card className="w-full flex flex-col">
       <CardHeader>
@@ -144,19 +90,5 @@ function PlanCard({ plan }: { plan: typeof plans[number] }) {
        
       </CardFooter>
     </Card>
-  )
-}
-
-export function SubscriptionPlansPopup() {
-  return (
-    <div className="container py-10">
-      <h1 className="text-3xl font-bold text-center mb-10">Planes de Suscripción</h1>
-      <div className="mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
-        {plans.map((plan, index) => (
-          <PlanCard key={index} plan={plan} />
-        ))}
-      </div>
-      <PlainsComparisonTable/>
-    </div>
   )
 }
