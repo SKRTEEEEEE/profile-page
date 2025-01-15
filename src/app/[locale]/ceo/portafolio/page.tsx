ac2364406935609fef756bee64f2e5acca766804
SKRTEEEEEE <adanreh.m@gmail.com>
2025-01-15 15:26:40 +0100

    refactor(ceo): change layout for all handle same bg and unifiqued main p2

--- src/app/[locale]/ceo/portafolio/page.tsx
+++ src/app/[locale]/ceo/portafolio/page.tsx
@@ -11,7 +11,7 @@ const PortfolioPage = async () => {
     const t = await getTranslations("ceo")
     const dataPortfolio = await getProjects()
     return (
-        <main className="w-full max-w-6xl px-4 pb-40 mx-auto mt-40 md:pb-0 md:px-6">
+        <main className="max-w-6xl px-4 pb-40 mx-auto mt-40 md:mt-6 md:pb-0 md:px-6">
             <TransitionPage />
             <MotionTransition position='bottom' className="bottom-0 left-0 hidden xl:inline-block xl:absolute ">
                 <Image src="/ceo/avatar-works.png" width="200" height="300" className="w-full h-full " alt={t("images.tablet")} />
