a7269f19506449fca2423cbcc67658358098310d
SKRTEEEEEE <adanreh.m@gmail.com>
2025-02-01 12:34:33 +0100

    refactor(ceo): Edit err mobile and desktop views in info

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,4 @@
-import { TestAction } from "@/components/ceo/test-action-project";
+// import { TestAction } from "@/components/ceo/test-action-project";
 import { ToastTriggered } from "@/components/oth/toast-triggered";
 import { Link } from "@/i18n/routing";
 import { getTranslations } from "next-intl/server";
@@ -24,7 +24,7 @@ export default async function Home({searchParams}:RootProps) {
           title: `${t("manage-root.title")} 🏗️`, 
           description: t("manage-root.desc"), 
           duration: 10000}}/>
-        <TestAction/>
+        {/* <TestAction/> */}
       </div>
     </main>
   );
--- src/components/ceo/counter-services.tsx
+++ src/components/ceo/counter-services.tsx
@@ -33,14 +33,14 @@ const dataCounter = [
 const CounterServices = () => {
     const t = useTranslations("ceo.estudios.counter")
     return (
-        <div className="grid justify-between max-w-3xl grid-cols-2 gap-3  md:grid-cols-3 md:gap-6">
+        <div className="grid justify-between max-w-3xl grid-cols-2 gap-2  md:grid-cols-3 md:gap-6">
             {dataCounter.map(({ id, endCounter, lineRight, lineRightMobile }) => (
                 <div key={id} className={`${lineRight && 'ltr'}`}>
-                    <h2 tabIndex={0} className={cn(`${lineRight && 'px-4 border-2 border-transparent md:border-e-gray-100'} ${lineRightMobile && 'border-e-gray-100'}`, id===2?"gap-4 flex md:grid md:gap-0 ml-4":"")} >
-                        <span className="flex mb-2 text-2xl font-extrabold md:text-4xl text-secondary-ceo">
+                    <h2 tabIndex={0} className={cn(`sm:mt-2 ${lineRight && 'px-2 sm:px-4 border-2 border-transparent md:border-e-gray-100'} ${lineRightMobile && 'border-e-gray-100'}`, id===2?"gap-4 flex md:grid md:gap-0 ml-4":"")} >
+                        <span className="flex sm:mb-2 text-2xl font-extrabold md:text-4xl text-secondary-ceo">
                             + <CountUp end={endCounter} start={0} duration={5} />
                         </span>
-                        <span className="text-xs uppercase max-w-[100px]">
+                        <span className={cn("text-xs sm:text-base uppercase sm:max-w-[100px]", id===2?" max-sm:min-w-full max-sm:mt-2":"")}>
                             {t(`${id}`)}
                         </span>
                     </h2>
